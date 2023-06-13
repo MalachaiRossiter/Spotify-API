eb2010f64de752452043fb25850780794daa5fcb
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-06-13 10:12:35 -0400

    hid keys in env file

--- server/controllers/spotify.controller.js
+++ server/controllers/spotify.controller.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports.spotifyArtist = (req, res) => {
-    axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials&client_id=e8707217fb8b4cdd98c6bd135fd412a6&client_secret=0642fcfadc1d4862b479b9a04ba31c23',
+    axios.post(`https://accounts.spotify.com/api/token', 'grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
     {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
     .then(artist => {
         console.log(artist);
