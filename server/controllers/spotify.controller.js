b5d2b5125d88f0c08a2acb3251e3f867b425d6a5
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-06-12 22:52:33 -0400

    fixed route

--- server/controllers/spotify.controller.js
+++ server/controllers/spotify.controller.js
@@ -1,11 +1,11 @@
 const axios = require('axios');
 
 module.exports.spotifyArtist = (req, res) => {
-    axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret',
+    axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials&client_id=e8707217fb8b4cdd98c6bd135fd412a6&client_secret=0642fcfadc1d4862b479b9a04ba31c23',
     {headers: {'Content-Type': 'application/x-www-form-urlencoded'}})
     .then(artist => {
         console.log(artist);
-        res(artist.data);
+        res.status(200).json(artist.data);
     })
     .catch((err) => {console.log(err)});
 }
