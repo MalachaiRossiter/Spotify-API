28dfe2de0f3972cc334be3c7af9f0f0f348c9b62
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-06-17 15:12:48 -0400

    tried new fixes to cors error

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const cors = require('cors');
+const cors=require("cors");
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
 const app = express();
 
 require('dotenv').config();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors(corsOptions));
+
+// Add a middleware to set the Access-Control-Allow-Origin header
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
