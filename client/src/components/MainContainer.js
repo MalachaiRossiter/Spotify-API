c04b65e237b055cd2f15e01dfe72962011ce164a
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-06-26 22:46:50 -0400

    added conditional rendering

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -2,8 +2,19 @@ import SearchButton from "./SearchButton";
 import { useLocation } from 'react-router-dom';
 
 const MainContainer = (props) => {
-    return (
-        <SearchButton/>
-    )
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const code = queryParams.get('code');
+
+    if (code) {
+        return (
+            <SpotifyInformation/>
+        )
+    }
+    else {
+        return (
+            <SearchButton/>
+        )
+    }
 }
 export default MainContainer;
