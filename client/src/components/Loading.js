ae11966fa5fbb0b82f5bd0f28b823f7b9086d2f3
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-07-20 12:57:17 -0400

    finished loading and session token

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -5,8 +5,11 @@ const Loading = (props) => {
     return (
         <div className={'loading-container'}>
             <div className={'music-container'}>
-                <img src={musicNote} alt={'music note'}/>
+                <img src={musicNote} alt={'music note'} className={'musicNote animate-opacity1'} id={'note1'}/>
+                <img src={musicNote} alt={'music note'} className={'musicNote animate-opacity2'} id={'note2'}/>
+                <img src={musicNote} alt={'music note'} className={'musicNote animate-opacity3'} id={'note3'}/>
             </div>
+            <h1>Loading...</h1>
         </div>
     )
 }
--- client/src/components/SpotifyInformation.js
+++ client/src/components/SpotifyInformation.js
@@ -3,27 +3,46 @@ import { useEffect, useState } from "react";
 import Loading from './Loading';
 
 const SpotifyInformation = (props) => {
-    const {spotifyTokenCode} = props;
+    const { spotifyTokenCode: initialToken } = props;
     const [userInformation, setUserInformation] = useState();
     const [loaded, setLoaded] = useState(false);
+    const [spotifyTokenCode, setSpotifyTokenCode] = useState(initialToken);
 
     useEffect(() => {
-        axios.post(`http://localhost:8000/api/spotify/token`, {code: spotifyTokenCode})
-        .then(res => {
-            console.log(res.data);
-            setLoaded(true);
-        })
-        .catch(err => {console.log(err)})
-    }, [spotifyTokenCode])
+        // Save the token to localStorage whenever it changes
+        localStorage.setItem("spotifyToken", spotifyTokenCode);
+
+        if (spotifyTokenCode) {
+            axios.post(`http://localhost:8000/api/spotify/token`, { code: spotifyTokenCode })
+                .then(res => {
+                    console.log(res.data);
+                    setUserInformation(res.data); // Save response data
+                    setLoaded(true);
+                })
+                .catch(err => {
+                    console.log(err);
+                    setLoaded(true); // Set loaded to true even on error to avoid infinite loop
+                });
+        }
+    }, [spotifyTokenCode]);
+
+    useEffect(() => {
+        // If the initialToken (from props) is different from the one in state,
+        // update the state with the initial token.
+        if (initialToken && initialToken !== spotifyTokenCode) {
+            setSpotifyTokenCode(initialToken);
+        }
+    }, [initialToken, spotifyTokenCode]);
 
     return (
         loaded ? (
             <div>
-                <h1>Penis</h1>
+                <h1>Penis</h1> {/* Just a placeholder text, you can replace it with your actual UI */}
             </div>
         ) : (
-            <Loading/>
+            <Loading />
         )
     )
 }
+
 export default SpotifyInformation;
